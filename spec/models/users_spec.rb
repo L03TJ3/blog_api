399801d231bcf6e956adaaedc7fcc61b1ae4a586
Laurence <laurenceweening@gmail.com>
2016-05-09 16:38:57 +0200

    Add test for validations on user model

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 describe User do
   describe "validations" do
+    it { should validates_presence_of :f_name }
+    it { should validates_presence_of :l_name }
+    it { should validates_presence_of :city }
 
-    it "requires a f_name/l_name/city/email/password"  do
-      
-    end
+    it { should validates_presence_of :email }
+    it { should validates_uniqueness_of :email }
   end
 end
